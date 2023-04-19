e4f24f98864d58838bbada80d565a8ee76ca55a7
MinhoYoo1989 <minhoyoo1989@neighbor.co.kr>
2023-04-19 13:16:01 +0900

    기능부분 로직 전부 구현 -완성- 테스트 환경에 로직 적용해보기

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,22 +1,51 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { List } from 'react-virtualized';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
 const TodoList = ({ todos, onRemove, onToggle }) => {
-  return (
-    <div className="TodoList">
-      {todos.map((todo) => (
+  const rowRenderer = useCallback(
+    ({ index, key, style }) => {
+      const todo = todos[index];
+
+      return (
         <TodoListItem
+          className="TodoList"
           todo={todo}
-          key={todo.id}
+          key={key}
           onRemove={onRemove}
           onToggle={onToggle}
-        />
-      ))}
-      {/* <TodoListItem />
-      <TodoListItem />
-      <TodoListItem /> */}
-    </div>
+          style={style}
+        >
+          {/* {todos.map((todo) => (
+            <TodoListItem
+              todo={todo}
+              key={key}
+              onRemove={onRemove}
+              onToggle={onToggle}
+              style={style}
+            />
+          ))} */}
+          {/* <TodoListItem />
+        <TodoListItem />
+        <TodoListItem /> */}
+        </TodoListItem>
+      );
+    },
+    [onRemove, onToggle, todos],
+  );
+
+  return (
+    <List
+      className="TodoList"
+      width={512} // 전체크기
+      height={513} //전체높이
+      rowCount={todos.length} //항목 갯수
+      rowHeight={57} // 항목 높이
+      rowRenderer={rowRenderer} //항목을 렌더링 할 때 쓰는 함수
+      List={todos} // 배열
+      style={{ outline: 'none' }} // List에 기본 적용 되는 outline 스타일 제거
+    />
   );
 };
 
--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -7,20 +7,25 @@ import {
 import cn from 'classnames';
 import './TodoListItem.scss';
 
-const TodoListItem = ({ todo, onRemove, onToggle }) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
   const { id, text, checked } = todo; //asd
 
   return (
-    <div className="TodoListItem">
-      {/* <div className="checkbox"> */}
-      <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
-        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-        {/* <MdCheckBoxOutlineBlank />
+    <div className="TodoListItem-virtualized" style={style}>
+      <div className="TodoListItem">
+        {/* <div className="checkbox"> */}
+        <div
+          className={cn('checkbox', { checked })}
+          onClick={() => onToggle(id)}
+        >
+          {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+          {/* <MdCheckBoxOutlineBlank />
         <div className="text">할 일</div> */}
-        <div className="text">{text}</div>
-      </div>
-      <div className="remove" onClick={() => onRemove(id)}>
-        <MdRemoveCircleOutline />
+          <div className="text">{text}</div>
+        </div>
+        <div className="remove" onClick={() => onRemove(id)}>
+          <MdRemoveCircleOutline />
+        </div>
       </div>
     </div>
   );
