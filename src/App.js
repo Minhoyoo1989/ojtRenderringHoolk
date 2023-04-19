e12b03e05b50210c264b0f4fd5ddbeaa90ddbde9
MinhoYoo1989 <minhoyoo1989@neighbor.co.kr>
2023-04-19 09:20:40 +0900

    1차 기능 부분 예제 항목 업데이트, 책에서는 체크박스 체크되면 취소선이 그어져 나오는데 책 대로 써도 그대로 안됨

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
+import { useState } from 'react';
 // import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '리액트의 기초를 알아보기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '컴포넌트로 스타일링 해 보기',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '일정 관리 앱 만들어 보기',
+      checked: false,
+    },
+  ]);
+
   return (
     <TodoTemplate>
-      TODO App을 만들자!
+      {/* TODO App을 만들자! */}
       <TodoInsert />
-      <TodoList />
+      <TodoList todos={todos} />
     </TodoTemplate>
     // <div className="App">
     //   <header className="App-header">
